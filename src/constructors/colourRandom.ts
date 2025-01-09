318867430dc5d2132bfa6502e864dda4dd06bb16
maxitect <maxime.downe@gmail.com>
2025-01-09 16:52:04 +0000

    chore: add colour constructors from different arguments (hex, hsl, luminance, none)

--- src/constructors/colourRandom.ts
+++ src/constructors/colourRandom.ts
@@ -1,12 +1,17 @@
 import { Colour } from "@/types/Colour";
-import { HSL } from "@/types/colourTypes";
 
 import hslToRgb from "@/utils/hslToRgb";
+import randomInRange from "@/utils/randomInRange";
 import rgbToHex from "@/utils/rgbToHex";
 import rgbToLuminance from "@/utils/rgbToLuminance";
 import rgbToXyz from "@/utils/rgbToXyz";
 
-export default function colourByHex(hsl: HSL): Colour {
+export default function colourRandom(): Colour {
+  const hsl = {
+    h: randomInRange(0, 1),
+    s: randomInRange(0.6, 1),
+    l: randomInRange(0.3, 0.7),
+  };
   const rgb = hslToRgb(hsl);
   const luminance = rgbToLuminance(rgb);
   return {
